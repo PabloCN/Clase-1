d153d7444cd1ff713bac6b37e0e5e09eee3e2e6f
PabloCN <hose.paburo.999@gmail.com>
2017-02-15 08:32:36 -0600

    Add files via upload

--- MiProgramaOO.java
+++ MiProgramaOO.java
@@ -16,11 +16,30 @@ public class MiProgramaOO {
      */
     public static void main(String[] args) {
         
-        Persona juan= null;
+        //Persona juan= null;
         Persona juan = new Persona();
+        juan.nombre= "Juan";
         juan.edad=23;
+        juan.peso=87.5;
+        juan.estatura= 1.55;
+        juan.sexo= 'm';
+        juan.procedencia="Hacienda Nueva";
         Persona maria = new Persona();
+        maria.nombre= "Maria";
+        maria.edad=18;
+        maria.peso=67.5;
+        maria.estatura= 1.70;
+        maria.sexo= 'f';
+        maria.procedencia="El Cobre";
         Persona miguelito = new Persona();
+        miguelito.nombre= "Miguelito";
+        miguelito.edad=12;
+        miguelito.peso=40.6;
+        miguelito.estatura= 1.4;
+        miguelito.sexo= 'm';
+        miguelito.procedencia="Huehuquilla";
+        
+        System.out.println();
         
     }
     
